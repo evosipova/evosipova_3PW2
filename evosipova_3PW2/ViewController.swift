0648ab436e93bcf18e14a137414b577c7a1c1103
evosipova <90273831+evosipova@users.noreply.github.com>
2022-10-08 00:53:56 +0300

    Initial Commit

---
+++ evosipova_3PW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  evosipova_3PW2
+//
+//  Created by Elizabeth on 08.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
